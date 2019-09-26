fa9b4f174a54c694628ead2d5e55c7ffa38670aa
yowcow <yowcow@cpan.org>
2019-09-26 19:30:26 +0900

    update

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [L_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        KC_DEL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    FNS_L,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB ,                        KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
+    XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB ,                        KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
     FN_L,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LGUI,                        KC_RGUI, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_QUOT, \
     KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
-    KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX,          KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , KC_RCTL,          KC_PGUP, KC_PGDN, XXXXXXX, KC_RALT  \
+    KC_LALT, XXXXXXX, XXXXXXX, KC_LGUI,          KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , KC_RCTL,          KC_RGUI, KC_PGUP, KC_PGDN, KC_RALT  \
   ),
 
   /* Lower
@@ -58,6 +58,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                        _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
     _______, VOL_DOWN,VOL_UP,  VOL_MUTE,XXXXXXX, XXXXXXX, _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                        _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, \
-    _______, _______, _______, _______,          _______, _______, _______,      _______, _______, _______,          KC_HOME, KC_END , _______, _______  \
+    _______, _______, _______, _______,          _______, _______, _______,      _______, _______, _______,          _______, KC_HOME, KC_END,  _______  \
   )
 };
