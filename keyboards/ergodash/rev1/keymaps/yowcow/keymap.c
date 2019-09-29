6ee690a0b91ae175c2bd432a095c3ff108be749f
yowcow <yowcow@cpan.org>
2019-09-29 17:11:13 +0900

    brackets in the middle

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        KC_DEL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB ,                        KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
-    FN_L,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LGUI,                        KC_RGUI, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_QUOT, \
+    FN_L,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_QUOT, \
     KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
     KC_LALT, XXXXXXX, XXXXXXX, KC_LGUI,          KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , KC_RCTL,          KC_RGUI, KC_PGUP, KC_PGDN, KC_RALT  \
   ),
