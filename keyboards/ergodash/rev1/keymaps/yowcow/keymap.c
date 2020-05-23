e4dc6496fc691252b4712c899157bf13b6a8657e
yowcow <yowcow@cpan.org>
2020-05-23 18:50:27 +0900

    tweak

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [L_QWERTY] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        KC_DEL , KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC ,                        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB ,                        KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
     FN_L,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LGUI,                        KC_RGUI, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_QUOT, \
     KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
-    KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX,          KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , KC_RCTL,          XXXXXXX, KC_PGUP, KC_PGDN, KC_RALT  \
+    KC_LALT, XXXXXXX, XXXXXXX,     KC_LALT,      KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , KC_RCTL,      KC_RALT,     KC_PGUP, KC_PGDN, KC_RALT  \
   ),
 
   /* Lower
@@ -54,8 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [L_FUNCT] = LAYOUT(
-    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                        _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
+    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        KC_DEL , KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+    _______, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, _______,                        _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, _______, \
     _______, VOL_DOWN,VOL_UP,  VOL_MUTE,XXXXXXX, XXXXXXX, _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                        _______, XXXXXXX, XXXXXXX, _______, _______, _______, _______, \
     _______, _______, _______, _______,          _______, _______, _______,      _______, _______, _______,          _______, KC_HOME, KC_END,  _______  \
