bbcf63521f2e6f7a84b643d5e51f4a48f22ab413
yowcow <yowcow@x28.co>
2020-06-02 13:27:24 +0900

    tweak

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,                        XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,  \
     XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB ,                        KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, \
-    XXXXXXX, RAISE,   KC_ESC,      KC_LGUI,      KC_LCTL, KC_SPC , KC_LSFT,      KC_BSPC, KC_ENT , KC_RALT,    XXXXXXX,       XXXXXXX, LOWER,   XXXXXXX  \
+    XXXXXXX, RAISE,   KC_LGUI,     XXXXXXX,      KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , KC_RALT,    XXXXXXX,       KC_BSPC, LOWER,   XXXXXXX  \
   ),
 
   [L_UPPER] = LAYOUT(
