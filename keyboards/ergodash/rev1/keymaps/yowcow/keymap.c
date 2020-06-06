820b8a5afaeb55cb2ccfba68437055cc1fc7cbff
Yoko OYAMA <yowcow@x28.co>
2020-06-06 12:42:23 +0900

    fix

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
     _______, KC_EXLM, KC_AT  , KC_UP  , KC_DLR , KC_PERC, _______,                        _______, KC_PGUP, KC_7   , KC_8   , KC_9   , KC_BSPC, _______, \
     _______, KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN, KC_CIRC,                        KC_AMPR, KC_PGDN, KC_4   , KC_5   , KC_6   , _______, _______, \
-    _______, KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RBRC, _______,                        _______, KC_ASTR, KC_1   , KC_2   , KC_3   , KC_PPLS, _______, \
+    _______, KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, _______,                        _______, KC_ASTR, KC_1   , KC_2   , KC_3   , KC_PPLS, _______, \
     _______, RAISE  , _______, _______,          _______, _______, _______,      _______, _______, _______,          KC_PDOT, KC_0   , KC_PEQL, _______  \
   ),
 
