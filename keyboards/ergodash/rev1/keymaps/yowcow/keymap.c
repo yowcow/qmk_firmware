e4acaf39df601a4bb01b9d5464eaa590af3c325e
Yoko OYAMA <yowcow@x28.co>
2020-06-06 13:30:19 +0900

    tidy

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_FN] = LAYOUT( \
     _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______, \
     _______, KC_EXLM, KC_AT  , KC_UP  , KC_DLR , KC_PERC, _______,                        _______, KC_PGUP, KC_7   , KC_8   , KC_9   , KC_BSPC, _______, \
-    _______, KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN, KC_CIRC,                        KC_AMPR, KC_PGDN, KC_4   , KC_5   , KC_6   , KC_RGUI, _______, \
+    _______, KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN, KC_CIRC,                        KC_AMPR, KC_PGDN, KC_4   , KC_5   , KC_6   , _______, _______, \
     _______, KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, _______,                        _______, KC_ASTR, KC_1   , KC_2   , KC_3   , KC_PPLS, _______, \
     _______, RAISE  , _______, _______,          _______, _______, _______,      _______, _______, _______,          KC_PDOT, KC_0   , KC_PEQL, _______  \
   ),
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_RS] = LAYOUT( \
     _______, _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______, \
     _______, KC_INS , KC_HOME, XXXXXXX, KC_END , KC_PGUP, _______,                        _______, KC_UP  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______, \
-    _______, KC_DEL,  VL_DOWN, VL_UP  , VL_MUTE, KC_PGDN, RESET  ,                        _______, KC_DOWN, KC_F4  , KC_F5  , KC_F6  , KC_F11 , _______, \
+    _______, KC_DEL , VL_DOWN, VL_UP  , VL_MUTE, KC_PGDN, RESET  ,                        _______, KC_DOWN, KC_F4  , KC_F5  , KC_F6  , KC_F11 , _______, \
     _______, XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, _______,                        _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F12 , _______, \
     _______, _______, _______, _______,          _______, _______, _______,      _______, _______, _______,          KC_PSCR, KC_SLCK, _______, _______  \
   )
