6892fa94337ad7a47a22fd01119f517885a79c48
Yoko OYAMA <yowcow@x19.dev>
2020-09-18 13:14:19 +0900

    tweak

--- keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
+++ keyboards/ergodash/rev1/keymaps/yowcow/keymap.c
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_QW] = LAYOUT( \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,                        XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX, \
-    XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB ,                        KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX, \
-    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, \
-    XXXXXXX, KC_ESC,  KC_LALT, KC_LGUI,          KC_LCTL, KC_SPC , KC_LSFT,      FUNCT  , KC_ENT , KC_RCTL,          KC_MINS, KC_QUOT, KC_BSLS, XXXXXXX  \
+    XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,                        XXXXXXX, KC_Y   , KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX, \
+    XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_TAB ,                        KC_BSPC, KC_H   , KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX, \
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N   , KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, \
+    XXXXXXX, KC_ESC,  KC_LALT, KC_LGUI,          KC_LCTL, KC_SPC , KC_LSFT,      KC_RSFT, KC_ENT , FUNCT  ,          KC_MINS, KC_QUOT, KC_BSLS, XXXXXXX  \
   ),
 
   [L_FN] = LAYOUT( \
