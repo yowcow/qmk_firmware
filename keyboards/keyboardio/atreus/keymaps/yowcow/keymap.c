afd01d4f076156a99e67aa8d271f09e1e9b01d2c
Yoko OYAMA <yowcow@gmail.com>
2021-04-22 23:44:46 +0900

    pinky quot

--- keyboards/keyboardio/atreus/keymaps/yowcow/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/yowcow/keymap.c
@@ -16,13 +16,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB,  KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC,  KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,  KC_LSFT, KC_RCTL, KC_ENT,  MO(_RS), KC_MINS, KC_QUOT, KC_BSLS ),
+    KC_ESC,  KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,  KC_LSFT, KC_RCTL, KC_ENT,  MO(_RS), KC_MINS, KC_GRV , KC_BSLS ),
 
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                   KC_PGUP, KC_7,    KC_8,    KC_9,    KC_PLUS,
     KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                   KC_PGDN, KC_4,    KC_5,    KC_6,    KC_EQL ,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_1,    KC_2,    KC_3,    KC_DOT ,
-    MO(_LW), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_GRV ),
+    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_1,    KC_2,    KC_3,    KC_QUOT,
+    MO(_LW), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    _______),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
